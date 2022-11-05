1f296411ad78e4ef561aed2da63d24e09a4d81f4
nelpno <nelpno@gmail.com>
2022-11-05 12:16:46 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='171261',
+    database='bdcrud',
+)
+
+cursor = conexao.cursor()
+
+# CRUD
+valor = 6
+nome_produto = "todynho"
+comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
+cursor.execute(comando)
+conexao.commit()  # edita o banco de dados
+
+cursor.close()
+conexao.close()
+
+# CREATE
+# nome_produto = "chocolate"
+# valor = 15
+# comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+# cursor.execute(comando)
+# conexao.commit() # edita o banco de dados
+
+
+# READ
+# comando = f'SELECT * FROM vendas'
+# cursor.execute(comando)
+# resultado = cursor.fetchall() # ler o banco de dados
+# print(resultado)
+
+
+# UPDATE
+# valor = 6
+# nome_produto = "todynho"
+# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit()  # edita o banco de dados
+
+
+# DELETE
+# valor = 6
+# nome_produto = "todynho"
+# comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit()  # edita o banco de dados
